3ec4aff86f01cc563cc402efbba3eedd9bcf021b
chinggg <24590067+chinggg@users.noreply.github.com>
2022-03-09 07:21:10 +0800

    week1: part1 and part2

--- week1/part-2-warmup/src/main.rs
+++ week1/part-2-warmup/src/main.rs
@@ -3,19 +3,38 @@
 use std::collections::HashSet;
 
 fn main() {
+    let mut v = vec![3, 1, 0, 1, 4, 4];
+    dedup(&mut v);
+    println!("{:?}", v);
     println!("Hi! Try running \"cargo test\" to run tests.");
 }
 
 fn add_n(v: Vec<i32>, n: i32) -> Vec<i32> {
-    unimplemented!()
+    let mut v2 = v.clone();
+    for x in v2.iter_mut() {
+        *x += n;
+    }
+    v2
 }
 
 fn add_n_inplace(v: &mut Vec<i32>, n: i32) {
-    unimplemented!()
+    for x in v.iter_mut() {
+        *x += n;
+    }
 }
 
 fn dedup(v: &mut Vec<i32>) {
-    unimplemented!()
+    let mut seen = HashSet::new();
+    let mut cnt = 0;
+    for i in 0..v.len() {
+        if seen.contains(&v[i - cnt]) {
+            println!("Removing {} {}, cnt: {}", i - cnt, v[i - cnt], cnt);
+            v.remove(i - cnt);
+            cnt += 1;
+        } else {
+            seen.insert(v[i - cnt]);
+        }
+    }
 }
 
 #[cfg(test)]
