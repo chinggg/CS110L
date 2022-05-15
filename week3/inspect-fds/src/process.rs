ce472f51dfaa5c690bedd3d781e24ed18d3319a8
chinggg <24590067+chinggg@users.noreply.github.com>
2022-05-15 12:31:28 +0800

    week3: inspect-fds milestone4
    print additional fdinfo

--- week3/inspect-fds/src/process.rs
+++ week3/inspect-fds/src/process.rs
@@ -15,7 +15,25 @@ impl Process {
 
     pub fn print(&self) {
         println!("========== \"{}\" (pid {}, ppid {}) ==========", self.command, self.pid, self.ppid);
-        println!("fds {:?} ", self.list_fds().unwrap());
+        match self.list_open_files() {
+            None => println!(
+                "Warning: could not inspect file descriptors for this process! \
+                It might have exited just as we were about to look at its fd table, \
+                or it might have exited a while ago and is waiting for the parent \
+                to reap it."
+            ),
+            Some(open_files) => {
+                for (fd, file) in open_files {
+                    println!(
+                        "{:<4} {:<15} cursor: {:<4} {}",
+                        fd,
+                        format!("({})", file.access_mode),
+                        file.cursor,
+                        file.colorized_name(),
+                    );
+                }
+            }
+        }
     }
 
     /// This function returns a list of file descriptor numbers for this Process, if that
@@ -28,7 +46,12 @@ impl Process {
         let mut fds = Vec::<usize>::new();
         for entry in fs::read_dir(path).ok()? {
             let entry = entry.ok()?;
-            let fd = entry.file_name().into_string().unwrap().parse::<usize>().ok()?;
+            let fd = entry
+                .file_name()
+                .into_string()
+                .unwrap()
+                .parse::<usize>()
+                .ok()?;
             fds.push(fd);
         }
         Some(fds)
@@ -37,7 +60,6 @@ impl Process {
     /// This function returns a list of (fdnumber, OpenFile) tuples, if file descriptor
     /// information is available (it returns None otherwise). The information is commonly
     /// unavailable if the process has already exited.
-    #[allow(unused)] // TODO: delete this line for Milestone 4
     pub fn list_open_files(&self) -> Option<Vec<(usize, OpenFile)>> {
         let mut open_files = vec![];
         for fd in self.list_fds()? {
