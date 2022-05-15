59f53d43118be4f1cfb4d87dd4b4476179a3ef39
chinggg <24590067+chinggg@users.noreply.github.com>
2022-05-16 00:01:13 +0800

    week3: generic linked_list with traits

--- week3/linked_list/src/main.rs
+++ week3/linked_list/src/main.rs
@@ -1,12 +1,14 @@
 use linked_list::LinkedList;
+
+use crate::linked_list::ComputeNorm;
 pub mod linked_list;
 
 fn main() {
-    let mut list: LinkedList<u32> = LinkedList::new();
+    let mut list = LinkedList::<f64>::new();
     assert!(list.is_empty());
     assert_eq!(list.get_size(), 0);
     for i in 1..12 {
-        list.push_front(i);
+        list.push_front(i as f64);
     }
     println!("{}", list);
     println!("list size: {}", list.get_size());
@@ -16,7 +18,8 @@ fn main() {
     println!("{}", list.to_string()); // ToString impl for anything impl Display
 
     // If you implement iterator trait:
-    //for val in &list {
-    //    println!("{}", val);
-    //}
+    for val in &list {
+       println!("{}", val);
+    }
+    println!("{}", list.compute_norm());
 }
