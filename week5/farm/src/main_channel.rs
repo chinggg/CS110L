7d2329c122e44b1342388e9a346086205489cb6e
chinggg <24590067+chinggg@users.noreply.github.com>
2022-06-11 14:57:37 +0800

    week5: farm channel version

---
+++ week5/farm/src/main_channel.rs
@@ -0,0 +1,93 @@
+use crossbeam_channel;
+use std::{collections::VecDeque, io::BufRead};
+#[allow(unused_imports)]
+use std::sync::{Arc, Mutex};
+use std::time::Instant;
+#[allow(unused_imports)]
+use std::{env, process, thread};
+
+/// Determines whether a number is prime. This function is taken from CS 110 factor.py.
+///
+/// You don't need to read or understand this code.
+#[allow(dead_code)]
+fn is_prime(num: u32) -> bool {
+    if num <= 1 {
+        return false;
+    }
+    for factor in 2..((num as f64).sqrt().floor() as u32) {
+        if num % factor == 0 {
+            return false;
+        }
+    }
+    true
+}
+
+/// Determines the prime factors of a number and prints them to stdout. This function is taken
+/// from CS 110 factor.py.
+///
+/// You don't need to read or understand this code.
+#[allow(dead_code)]
+fn factor_number(num: u32) {
+    let start = Instant::now();
+
+    if num == 1 || is_prime(num) {
+        println!("{} = {} [time: {:?}]", num, num, start.elapsed());
+        return;
+    }
+
+    let mut factors = Vec::new();
+    let mut curr_num = num;
+    for factor in 2..num {
+        while curr_num % factor == 0 {
+            factors.push(factor);
+            curr_num /= factor;
+        }
+    }
+    factors.sort();
+    let factors_str = factors
+        .into_iter()
+        .map(|f| f.to_string())
+        .collect::<Vec<String>>()
+        .join(" * ");
+    println!("{} = {} [time: {:?}]", num, factors_str, start.elapsed());
+}
+
+/// Returns a list of numbers supplied via argv.
+#[allow(dead_code)]
+fn get_input_numbers() -> VecDeque<u32> {
+    let mut numbers = VecDeque::new();
+    for arg in env::args().skip(1) {
+        if let Ok(val) = arg.parse::<u32>() {
+            numbers.push_back(val);
+        } else {
+            println!("{} is not a valid number", arg);
+            process::exit(1);
+        }
+    }
+    numbers
+}
+
+fn main() {
+    let start = Instant::now();
+    let (sender, receiver) = crossbeam_channel::unbounded();
+    let mut threads = Vec::new();
+    for _ in 0..num_cpus::get() {
+        let receiver = receiver.clone();
+        threads.push(thread::spawn(move || {
+            while let Ok(next_num) = receiver.recv() {
+                factor_number(next_num);
+            }
+        }));
+    }
+    let stdin = std::io::stdin();
+    for line in stdin.lock().lines() {
+        let num = line.unwrap().parse::<u32>().unwrap();
+        sender
+            .send(num)
+            .expect("Tried writing to channel, but there are no receivers!");
+    }
+    drop(sender);
+    for thread in threads {
+        thread.join().expect("Panic occurred in thread");
+    }
+}
